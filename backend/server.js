9e97d436292fcf9a1d3b62cb082b0d491c2ce19b
NIKITA <hirapnikitacs222317@gmail.com>
2026-08-25 13:30:29 +0530

    server js fixed

--- backend/server.js
+++ backend/server.js
@@ -19,13 +19,16 @@ const PORT = process.env.PORT || 5000;
 app.use(
   cors({
     origin: [
+      "https://brown-walrus-852933.hostingersite.com",
       "https://inkconvention.com",
       "https://www.inkconvention.com",
       "http://localhost:5173",
       "http://localhost:3000",
     ],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
-  }),
+  })
 );
 
 app.use(express.json());
